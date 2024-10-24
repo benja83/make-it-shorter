9cb4fa5aa2c19b08df7a9deeedcb877f0f080c11
benja83 <tarennebenjamin@gmail.com>
2024-10-24 22:41:06 +0200

    🎉Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+	kotlin("jvm") version "1.9.25"
+	kotlin("plugin.spring") version "1.9.25"
+	id("org.springframework.boot") version "3.3.5"
+	id("io.spring.dependency-management") version "1.1.6"
+	jacoco
+}
+
+group = "com.benja83"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+kotlin {
+	compilerOptions {
+		freeCompilerArgs.addAll("-Xjsr305=strict")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+	finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
+}
+
+tasks.jacocoTestReport {
+	dependsOn(tasks.test) // tests are required to run before generating the report
+}
---
+++ src/test/kotlin/com/benja83/urlShortener/UrlShortenerApplicationTests.kt
@@ -0,0 +1,15 @@
+package com.benja83.urlShortener
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import kotlin.test.assertTrue
+
+@SpringBootTest
+class UrlShortenerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+		assertTrue { true }
+	}
+
+}
