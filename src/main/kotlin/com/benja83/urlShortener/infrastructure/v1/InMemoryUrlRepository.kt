04ecc11fe9584e22c229099caa4c0cff73b0e9d5
benja83 <tarennebenjamin@gmail.com>
2024-10-27 11:48:03 +0100

    ♻️Store only Url domain object in repository

--- src/main/kotlin/com/benja83/urlShortener/infrastructure/v1/InMemoryUrlRepository.kt
+++ src/main/kotlin/com/benja83/urlShortener/infrastructure/v1/InMemoryUrlRepository.kt
@@ -2,24 +2,21 @@ package com.benja83.urlShortener.infrastructure.v1
 
 import com.benja83.urlShortener.UrlRepositoryInterface
 import com.benja83.urlShortener.domain.Url
-import java.net.URI
 
 typealias LongUrl = String
 typealias ShortUrlSuffix = String
 
 class InMemoryUrlRepository : UrlRepositoryInterface {
-    private val mapLongToShortUrlSuffix = HashMap<LongUrl, ShortUrlSuffix>()
+    private val mapLongToShortUrlSuffix = HashMap<LongUrl, Url>()
     private val mapShortUrlSuffixToLongUrl = HashMap<ShortUrlSuffix, Url>()
 
     override fun save(url: Url) {
-        mapLongToShortUrlSuffix[url.longUrl.toString()] = url.shortUrlSuffix
+        mapLongToShortUrlSuffix[url.longUrl.toString()] = url
         mapShortUrlSuffixToLongUrl[url.shortUrlSuffix] = url
     }
 
     override fun findByLongUrl(longUrl: String): Url? {
-        return mapLongToShortUrlSuffix[longUrl]?.let {
-            Url(URI(longUrl).toURL(), it)
-        }
+        return mapLongToShortUrlSuffix[longUrl]
     }
 
     override fun findByShortUrlSuffix(shortUrlSuffix: String): Url? {
